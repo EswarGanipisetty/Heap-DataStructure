65baaa438d5d45f875d994f6983cbf2385acc218
EswarGanipisetty <158088884+EswarGanipisetty@users.noreply.github.com>
2024-02-20 22:32:29 +0530

    Update heap.py

--- heap.py
+++ heap.py
@@ -3,64 +3,142 @@ class MinHeap:
         self.heap = []
 
     def parent(self, i):
-        # Bit manipulation: (i-1) >> 1 is equivalent to (i-1) // 2
         return (i - 1) >> 1
 
-    def left_child(self, i):
-        # Bit manipulation: (2*i) + 1 is equivalent to (2*i) << 1 | 1
-        return (i << 1) | 1
+    def left(self, i):
+        return (i << 1) + 1
 
-    def right_child(self, i):
-        # Bit manipulation: (2*i) + 2 is equivalent to (2*i) << 1 | 2
-        return (i << 1) | 2
+    def right(self, i):
+        return (i << 1) + 2
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def heapify_up(self, i):
+        while i > 0 and self.heap[self.parent(i)] > self.heap[i]:
+            self.swap(i, self.parent(i))
+            i = self.parent(i)
+
+    def heapify_down(self, i):
+        while True:
+            smallest = i
+            l = self.left(i)
+            r = self.right(i)
+
+            if l < len(self.heap) and self.heap[l] < self.heap[smallest]:
+                smallest = l
+
+            if r < len(self.heap) and self.heap[r] < self.heap[smallest]:
+                smallest = r
+
+            if smallest != i:
+                self.swap(i, smallest)
+                i = smallest
+            else:
+                break
 
     def insert(self, val):
         self.heap.append(val)
         self.heapify_up(len(self.heap) - 1)
 
-    def heapify_up(self, i):
-        while i != 0 and self.heap[self.parent(i)] > self.heap[i]:
-            self.heap[self.parent(i)], self.heap[i] = self.heap[i], self.heap[self.parent(i)]
-            i = self.parent(i)
-
     def extract_min(self):
-        if len(self.heap) == 0:
+        if not self.heap:
             return None
         if len(self.heap) == 1:
             return self.heap.pop()
 
-        min_val = self.heap[0]
+        root = self.heap[0]
         self.heap[0] = self.heap.pop()
         self.heapify_down(0)
-        return min_val
-
-    def heapify_down(self, i):
-        smallest = i
-        left = self.left_child(i)
-        right = self.right_child(i)
-
-        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
-            smallest = left
-
-        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
-            smallest = right
-
-        if smallest != i:
-            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
-            self.heapify_down(smallest)
+        return root
 
     def get_min(self):
-        if self.heap:
-            return self.heap[0]
-        return None
+        if not self.heap:
+            return None
+        return self.heap[0]
 
-
+# Example usage:
 heap = MinHeap()
-heap.insert(5)
 heap.insert(3)
-heap.insert(8)
+heap.insert(2)
 heap.insert(1)
+heap.insert(4)
+heap.insert(5)
+print(heap.extract_min())  # Output: 1
+print(heap.extract_min())  # Output: 2
+print(heap.extract_min())  # Output: 3
+print(heap.extract_min())  # Output: 4
+print(heap.extract_min())  # Output: 5
+print(heap.extract_min())  # Output: None (heap is empty)
+class MinHeap:
+    def __init__(self):
+        self.heap = []
 
-print("Minimum element:", heap.get_min())  
-print("Extracted min:", heap.extract_min())  
-print("Minimum element:", heap.get_min())  
+    def parent(self, i):
+        return (i - 1) >> 1
+
+    def left(self, i):
+        return (i << 1) + 1
+
+    def right(self, i):
+        return (i << 1) + 2
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def heapify_up(self, i):
+        while i > 0 and self.heap[self.parent(i)] > self.heap[i]:
+            self.swap(i, self.parent(i))
+            i = self.parent(i)
+
+    def heapify_down(self, i):
+        while True:
+            smallest = i
+            l = self.left(i)
+            r = self.right(i)
+
+            if l < len(self.heap) and self.heap[l] < self.heap[smallest]:
+                smallest = l
+
+            if r < len(self.heap) and self.heap[r] < self.heap[smallest]:
+                smallest = r
+
+            if smallest != i:
+                self.swap(i, smallest)
+                i = smallest
+            else:
+                break
+
+    def insert(self, val):
+        self.heap.append(val)
+        self.heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if not self.heap:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+        return root
+
+    def get_min(self):
+        if not self.heap:
+            return None
+        return self.heap[0]
+
+# Example usage:
+heap = MinHeap()
+heap.insert(3)
+heap.insert(2)
+heap.insert(1)
+heap.insert(4)
+heap.insert(5)
+print(heap.extract_min())  # Output: 1
+print(heap.extract_min())  # Output: 2
+print(heap.extract_min())  # Output: 3
+print(heap.extract_min())  # Output: 4
+print(heap.extract_min())  # Output: 5
+print(heap.extract_min())  # Output: None (heap is empty)
